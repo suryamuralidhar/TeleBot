06dc0916f07147f22b2dc04d62433e416e4e91a9
suryamuralidhar <suryamurali006@gmail.com>
2026-03-27 22:38:43 +0530

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -1,22 +1,5 @@
 from telethon import TelegramClient
-from telethon.sessions import StringSession
+from config import API_ID, API_HASH
 
-clients = []
-current_index = 0
-
-async def init_clients(accounts):
-    global clients
-    for acc in accounts:
-        client = TelegramClient(
-            StringSession(acc["session"]),
-            acc["api_id"],
-            acc["api_hash"]
-        )
-        await client.connect()
-        clients.append(client)
-
-def get_client():
-    global current_index
-    client = clients[current_index]
-    current_index = (current_index + 1) % len(clients)
-    return client
+def get_client(session_name="mysession"):
+    return TelegramClient(session_name, API_ID, API_HASH)
